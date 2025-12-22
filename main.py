ab25269e486687a478569be47279fa073651417e
thanasiscsd <thanasisgeorgopoulos2005@gmail.com>
2025-12-22 15:39:47 +0200

    Fix startup crash by replacing ft.colors with ft.Colors to support newer Flet versions

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ class MusicDownloaderApp:
         self.total_progress = ft.ProgressBar(
             width=1500,
             value=0,
-            color=ft.colors.INDIGO,
+            color=ft.Colors.INDIGO,
             bgcolor=None,
             bar_height=20
         )
@@ -179,7 +179,7 @@ class MusicDownloaderApp:
         self.file_progress = ft.ProgressBar(
             width=1500,
             value=0,
-            color=ft.colors.INDIGO,
+            color=ft.Colors.INDIGO,
             bgcolor=None,
             bar_height=20
         )
